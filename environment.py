bb3961ce93f3c93d8bd4f1a3509097359926b795
lucapierdicca <luca.pierdicca@gmail.com>
2019-09-18 22:21:39 +0200

    new_output

--- environment.py
+++ environment.py
@@ -73,7 +73,7 @@ class Grid:
             reward = 10.0
             done = 1.0
         else:
-            reward = -0.01
+            reward = -1
             done = 0.0
 
         return list(self.state), reward, done
--- main.py
+++ main.py
@@ -119,7 +119,7 @@ agent = DQN(env.n_actions,
     
 
 n_episode = 2000
-offset_train = 5
+offset_train = 15
 offset_copy = 40
 max_episode = 1000
 
