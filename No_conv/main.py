d3275fa6e200c678a6d44eb98eac11a66e9c7166
lucapierdicca <luca.pierdicca@gmail.com>
2020-03-24 13:51:55 +0100

    aggiunto no_conv

---
+++ No_conv/main.py
@@ -0,0 +1,120 @@
+from environment import Grid
+from network import DQN
+from collections import deque
+import pickle
+from time import gmtime, strftime
+
+
+
+def train_loop(n_episode, offset_train, offset_copy, max_episode):
+
+    global env
+
+    tot_step_counter=0
+    cost=0.0
+    #epsilon_counter = 0
+
+    histories = {'episode_reward':[],
+                 'max_Q':[],
+                 'cost':[],
+                 'epsilon':[],
+                 'episode_len':[]}
+
+    for episode in range(n_episode):
+
+        s = env.reset()
+
+        episode_step_counter=0.0 
+        episode_reward=0.0
+        episode_max_Q=0.0
+        episode_cost=0.0
+        #episode_epsilon=0.0
+
+        while True:
+
+            # env.render()
+            a, max_Q = agent.get_action(s)
+
+            s_, r, d = env.step(a)
+
+            # a transition is [[history],int,int,[history_],int]
+            agent.store_transition(s, a, r, s_, d)
+            # print("%d - %d - %d - %s - %s - %f - %s - %f - %f" % 
+            #     (episode, 
+            #      tot_step_counter, 
+            #      episode_step_counter, 
+            #      str(h), 
+            #      id_to_action[a], 
+            #      max_Q, 
+            #      str(h_), 
+            #      d, 
+            #      r))
+
+            if (tot_step_counter > 10000) and (tot_step_counter % offset_train == 0):
+                cost = agent.train(episode)
+                #print('********* TRAIN ********')
+
+            if (tot_step_counter > 10000) and (tot_step_counter % offset_copy == 0):
+                agent.copy_vars()
+                #print('********* COPY *********')
+
+            #episode_epsilon+=agent.epsilon
+            episode_max_Q+=max_Q
+            episode_reward+=r
+            episode_cost+=cost
+
+            episode_step_counter+=1
+            tot_step_counter += 1
+            
+   
+            if d or episode_step_counter == max_episode:
+                print("episode: %d - goal: %d (%d) - e: %.4f (%d) - Q: %.5f - loss: %.3f - reward: %.1f" % 
+                    (episode+1, d, episode_step_counter,
+                        episode_epsilon, tot_step_counter, 
+                        episode_max_Q/episode_step_counter, 
+                        episode_cost/episode_step_counter, 
+                        episode_reward/episode_step_counter))
+                histories['max_Q'].append(episode_max_Q/episode_step_counter)
+                histories['cost'].append(episode_cost/episode_step_counter)
+                histories['episode_reward'].append(episode_reward)
+                histories['epsilon'].append(episode_epsilon/episode_step_counter)
+                histories['episode_len'].append(episode_step_counter)
+                break
+        
+        if tot_step_counter > 20000:
+            #delta = n_episode-100
+            agent.epsilon = agent.epsilon - 0.9/200000 #max(((-0.9/delta)*epsilon_counter) + 1.0, 0.3)   
+            #epsilon_counter+=1
+            
+    pickle.dump(histories, open('histories.pickle','wb'))
+    print('Game over')
+    #agent.plot_cost()
+
+
+
+
+#--------------------------------------------
+
+env = Grid()
+
+
+agent = DQN(env.n_actions,
+            learning_rate=0.000001, #0.1
+            gamma=0.99,
+            epsilon=1.0,
+            memory_size=1000000,
+            batch_size=64,
+            hidden_units=128)
+    
+
+n_episode = 250000
+offset_train = 30
+offset_copy = 300
+max_episode = 1000
+
+train_loop(n_episode, 
+    offset_train, 
+    offset_copy,
+    max_episode)
+
+#DQN.eval()
---
+++ No_conv/network.py
@@ -0,0 +1,162 @@
+import numpy as np
+import tensorflow.compat.v1 as tf
+import tensorflow.contrib as contrib
+from collections import deque
+import random
+
+
+# Deep Q Network
+class DQN:
+    def __init__(
+            self,
+            n_actions,
+            learning_rate=0.01,
+            gamma=0.9,
+            epsilon=1.0,
+            memory_size=500,
+            batch_size=32,
+            hidden_units=256
+    ):
+        self.n_actions = n_actions
+        self.learning_rate = learning_rate
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.memory_size = memory_size
+        self.batch_size = batch_size
+        self.hidden_units = hidden_units
+
+        # experience replay memory
+        self.memory = deque([], maxlen=memory_size)
+
+        self.create_NNs()
+        self.saver = tf.train.Saver() #var_list=tf.get_default_graph().get_collection('trainable_variables')
+        self.sess = tf.Session()
+        self.sess.run(tf.global_variables_initializer())
+        self.copy_vars()
+
+
+        # tf.summary.FileWriter("logs/", self.sess.graph)
+
+        
+
+    def create_NNs(self):
+        # ------------------ Placeholders ------------------------
+        self.s = tf.placeholder(tf.float32, [None, 2], name='s')
+        self.s_ = tf.placeholder(tf.float32, [None, 2], name='s_')
+        self.r = tf.placeholder(tf.float32, [None, ], name='r')  
+        self.a = tf.placeholder(tf.int32, [None, ], name='a')  
+        self.d = tf.placeholder(tf.float32, [None, ], name='d')  
+
+               
+        # ------------------ Conv Q ------------------
+        with tf.variable_scope('Q'):
+            fc1_e = tf.layers.dense(self.s, self.hidden_units, tf.nn.relu, trainable=True)
+            self.q = tf.layers.dense(fc1_e, self.n_actions, trainable=True, name='q')
+
+
+        # ------------------ Conv Q_tgt ------------------
+        with tf.variable_scope('Q_tgt'):
+            fc1_t = tf.layers.dense(self.s_, self.hidden_units, tf.nn.relu, trainable=False)
+            self.q_tgt = tf.layers.dense(fc1_t, self.n_actions, trainable=False)
+
+
+        # --------------- Copying -------------------
+        self.copy_vars_op = [tf.assign(tgt, nor) for tgt, nor in zip(tf.trainable_variables('Q_tgt'), 
+            tf.trainable_variables('Q'))]
+
+
+        # ---------------- Training ---------------------
+        # reduce_max ritorna il massimo (wrt a) di q_tgt
+        target = self.r + (self.gamma*tf.reduce_max(self.q_tgt, axis=1)*(1-self.d))
+        #target = tf.stop_gradient(target)
+        a_indices = tf.stack([tf.range(tf.shape(self.a)[0], dtype=tf.int32), self.a], axis=1)
+        # in questo modo ottengo il valore di q dato lo stato e una specifica azione
+        q_wrt_a = tf.gather_nd(params=self.q, indices=a_indices)    # shape=(None, )
+        # la media delle squared differenceS (una per ogni elemento del batch che gli dai in input)
+        self.loss = tf.reduce_mean(tf.squared_difference(target, q_wrt_a))
+        
+        self.train_op = tf.train.RMSPropOptimizer(self.learning_rate).minimize(self.loss)
+
+        # export only Q graph (not Q_tgt)
+        tf.train.export_meta_graph(filename='./graph/graph.meta')
+
+
+    def copy_vars(self):
+        self.sess.run(self.copy_vars_op)
+
+    def store_transition(self, s, a, r, s_, d):
+        self.memory.append([s,a,r,s_,d])
+
+    def get_action(self, s):
+
+        # con Pr = epsilon scelgo random (uniformemente) tra tutte
+        # con Pr = 1-epsilon scelgo greedy
+
+        # get Q value for every action (array)
+        actions_value = self.sess.run(self.q, feed_dict={self.s: [s]})
+        max_Q = np.max(actions_value)
+        
+        if np.random.uniform() < self.epsilon:
+            # choose a random action
+            action = np.random.randint(0, self.n_actions)
+        else:
+            # choose the greedy action
+            argmax_Q = np.argmax(actions_value)
+            action = argmax_Q
+            
+        return action, max_Q
+
+    
+    def train(self,episode):
+
+        # sample batch of transition from memory
+        batch = random.sample(self.memory, self.batch_size)
+
+        batch_s, batch_a, batch_r, batch_s_, batch_d = [],[],[],[],[]
+        
+        for transition in batch:
+
+            batch_s.append(transition[0])
+            batch_a.append(transition[1])
+            batch_r.append(transition[2])
+            batch_s_.append(transition[3])
+            batch_d.append(transition[4]) 
+        
+
+        _, cost = self.sess.run(
+            [self.train_op, self.loss],
+            feed_dict={
+                self.s: batch_s,
+                self.a: batch_a,
+                self.r: batch_r,
+                self.s_: batch_s_,
+                self.d: batch_d
+            })
+
+        if episode % 10 == 0:
+            save_path = self.saver.save(self.sess, "./weights/weights.ckpt",
+                 global_step=episode, write_meta_graph=False)
+
+        return cost
+
+    def eval():
+        sess = tf.Session()
+        #tf.reset_default_graph()
+        saver = tf.train.import_meta_graph('./graph/graph.meta')
+        saver.restore(sess, tf.train.latest_checkpoint("./weights/"))
+
+
+
+        graph = tf.get_default_graph()
+        s = graph.get_tensor_by_name('s:0')
+        q = graph.get_tensor_by_name('Q/q/BiasAdd:0')
+
+        a = sess.run(q, {s:[[0,0]]})
+
+        print(a)
+
+
+
+
+if __name__ == '__main__':
+    pass
